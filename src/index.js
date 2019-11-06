e372846d722e7034bff49b3e6ac03075f3dd5292
flrngel <flrngel@gmail.com>
2019-11-06 01:09:42 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import unique from 'unique-selector';
+var $ = require('jquery');
+
+$('[href]').click(function(event) {
+  var url = [location.protocol, '//', location.host, location.pathname].join('');
+
+  gtag('event', 'click', {
+    'event_category': url,
+    'event_label': unique(event.target),
+    'value': 1
+  });
+});
